03edc99eb2353a592b0c2dca7a93ba9101f02747
AcePeQ <maciej.nojszewski22@gmail.com>
2025-07-19 14:17:59 +0200

    feat(ui): add InputRow, LoginForm

---
+++ frontend/src/components/InputRow/InputRow.tsx
@@ -0,0 +1,53 @@
+import {
+  cloneElement,
+  useState,
+  type InputHTMLAttributes,
+  type ReactElement,
+} from "react";
+import styles from "./InputRow.module.css";
+import { Eye, EyeOff } from "lucide-react";
+
+type InputRowProps = {
+  error?: string;
+  id: string;
+  label: string;
+  children: ReactElement<InputHTMLAttributes<HTMLInputElement>>;
+};
+
+function InputRow({ error, id, label, children }: InputRowProps) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  function togglePasswordVisibility() {
+    setShowPassword((prev) => !prev);
+  }
+
+  const childrenInput =
+    id === "password"
+      ? cloneElement(children, {
+          type: showPassword ? "text" : "password",
+        })
+      : children;
+
+  return (
+    <div className={styles.row}>
+      <label htmlFor={id} className={styles.label}>
+        {label}
+      </label>
+      <div className={styles.inputContainer}>
+        {id === "password" && (
+          <button
+            onClick={togglePasswordVisibility}
+            aria-label={showPassword ? "Show password" : "Hide password"}
+            className={styles.passwordButton}
+          >
+            {showPassword ? <EyeOff /> : <Eye />}
+          </button>
+        )}
+        {childrenInput}
+      </div>
+      {error && <p className={styles.error}>{error}</p>}
+    </div>
+  );
+}
+
+export default InputRow;
--- frontend/src/features/Auth/LoginForm/LoginForm.tsx
+++ frontend/src/features/Auth/LoginForm/LoginForm.tsx
@@ -1,7 +1,45 @@
+import { useForm, type SubmitHandler } from "react-hook-form";
 import styles from "./LoginForm.module.css";
+import InputRow from "../../../components/InputRow/InputRow";
+
+type LoginInputs = {
+  email: string;
+  password: string;
+};
 
 function LoginForm() {
-  return <div>Login</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginInputs>();
+
+  const onSubmit: SubmitHandler<LoginInputs> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+      <InputRow label="Email" id="email" error={errors.email?.message}>
+        <input
+          {...register("email", {
+            required: "Email field is required",
+          })}
+          type="text"
+          id="email"
+        />
+      </InputRow>
+      <InputRow label="Password" id="password" error={errors.password?.message}>
+        <input
+          {...register("password", {
+            required: "Password field is required",
+          })}
+          type="password"
+          id="password"
+        />
+      </InputRow>
+    </form>
+  );
 }
 
 export default LoginForm;
