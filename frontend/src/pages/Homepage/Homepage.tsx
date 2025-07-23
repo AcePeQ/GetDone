f4c85bb7a1e3aa461c10f03571c67547aa33494f
AcePeQ <maciej.nojszewski22@gmail.com>
2025-07-23 17:19:08 +0200

    feat: add functionalty to check whether user is logged in, and if yes the user will always be on the GetDone boards page

--- frontend/src/pages/Homepage/Homepage.tsx
+++ frontend/src/pages/Homepage/Homepage.tsx
@@ -1,8 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import Logo from "../../components/Logo/Logo";
 import AuthPanel from "../../features/Auth/AuthPanel/AuthPanel";
 import styles from "./Homepage.module.css";
+import { useEffect } from "react";
+import { useUserStore } from "../../stores/useUserStore";
 
 function Homepage() {
+  const { user } = useUserStore();
+  const naviagate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      naviagate("/getdone", { replace: true });
+    }
+  }, [naviagate, user]);
+
   return (
     <div className={styles.wrapper}>
       <section className={styles.homepage}>
